1b9c48a101dfe15ea950f25c0ddc3782ba59242e
vinmugambi <vinmugambi17@gmail.com>
2021-04-14 09:57:50 +0300

    change eventseeder to use image links

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -20,7 +20,7 @@ class EventSeeder extends Seeder
             "description" => "The Mater Cardiac Program began 20 years ago, when The Mater Hospital realized that very little cardiac work of good quality was being carried out in the country. Patients who were financially endowed had to go overseas for treatment. Large majority, many of them children, had no recourse to quality Medicare and were destined to a sickly life and premature death.",
             "start_time" => "2021-07-22T08:00",
             "end_time" => "2021-07-22T17:00",
-            "image" => "images/matter.jpg",
+            "image" => "https://www.materkenya.com/images/gallery/gallery_14.jpg",
             "address" => "Matter Hospital",
             "city" => "Nairobi",
             "user_id" => 1
@@ -30,7 +30,7 @@ class EventSeeder extends Seeder
             "description" => "The Standard Chartered Bank, with the support of event organizers, Athletics Kenya, have built an event that has become internationally renowned and has placed Kenya on the international map. The Marathon is the flagship project for our ‘Futuremakers’ initiative; a new global campaign by Standard Chartered Bank to open opportunities for the next generation to learn, earn and grow.",
             "start_time" => "2021-10-10T08:00",
             "end_time" => "2021-10-10T17:00",
-            "image" => "images/nairobi.jpeg",
+            "image" => "https://www.nairobimarathon.com/wp-content/uploads/2017/07/7.jpg",
             "address" => "Standard Chartered",
             "city" => "Nairobi",
             "user_id" => 2
@@ -40,8 +40,8 @@ class EventSeeder extends Seeder
             "description" => "The Half marathon seeks to increase student sponsorship with a focus  on needy students enrolled for various courses offered in Kabarak University.",
             "start_time" => "2021-06-01T08:00",
             "end_time" => "2021-06-01T17:00",
-            "image" => "images/nairobi.jpg",
-            "address" => "Eldoret university",
+            "image" => "https://kabarak.ac.ke/halfmarathon/wp-content/uploads/2019/05/Half-Marathon.jpg",
+            "address" => "Kabarak University",
             "city" => "Eldoret",
             "user_id" => 3
         ]);
@@ -50,7 +50,7 @@ class EventSeeder extends Seeder
             "description" => "Eldoret City Marathon was founded in 2018 as a platform for launching the careers of young and unknown long-distance running talents while simultaneously promoting the conservation of the environment. Kenya is blessed with so many talented long distance runners that many potentially world-beating marathoners lack global opportunities due to being in the shadows of the more prominent world record holders, Olympians, world championship competitors, and those under the management of well-connected agents and/or managers.",
             "start_time" => "2021-06-03T08:00",
             "end_time" => "2021-06-03T17:00",
-            "image" => "images/eldoret.jpeg",
+            "image" => "https://www.eldoretcitymarathon.co.ke/ECM/images/men.jpg",
             "address" => "Eldoret University",
             "city" => "Eldoret University",
             "user_id" => 3
@@ -60,7 +60,7 @@ class EventSeeder extends Seeder
             "description" => "Run for Water, Run for Life acted as a clarion call for thousands of athletes who turned up at Ndakaini on 12th September 2015 for the 12th edition of Ndakaini Half Marathon. Well, some of the participants were not athletes but their love for nature drove them to support the call to conserve the Ndakaini Dam, a major source of water for Nairobi County.",
             "start_time" => "2021-04-03T08:00",
             "end_time" => "2021-04-03T17:00",
-            "image" => "images/ndakaini.jpeg",
+            "image" => "https://3dwnh01icn0h133s00sokwo1-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/Ndakaini-Half-Marathon.jpeg",
             "address" => "Ndakaini",
             "city" => "Murang'a",
             "user_id" => 1
@@ -70,7 +70,7 @@ class EventSeeder extends Seeder
             "description" => "In 2011, the Amazing Maasai ULTRA kicked off its inaugural event with about 100 local Maasai and 18 runners from abroad, mostly our friends we begged to come",
             "start_time" => "2021-08-14T08:00",
             "end_time" => "2021-08-14T17:00",
-            "image" => "images/maasai.jpg",
+            "image" => "https://www.amazinggirls.org/uploads/1/1/7/5/117558885/amazing-girls-books-5_orig.jpg",
             "address" => "Narok",
             "city" => "Narok",
             "user_id" => 3
